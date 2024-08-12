98eb3dddb9dfb2ee9a08ee6322eda8b647818eaf
tiw <tika11124@gmail.com>
2024-08-12 13:11:03 +0500

    add: edit Employee function

--- src/employees/serializers/employee_serializers.py
+++ src/employees/serializers/employee_serializers.py
@@ -9,3 +9,23 @@ class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
         fields = ['id', 'first_name', 'last_name', 'phone']
+
+    # Обновление пароля
+    def update(self, instance, validated_data):
+        # Обновляем данные сотрудника
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+
+        # Обновляем номер телефона пользователя
+        user_data = validated_data.get('user', {})
+        phone = user_data.get('phone')
+        if phone:
+            # Проверяем, что номер телефона уникален
+            if CustomUser.objects.filter(phone=phone).exclude(id=instance.user.id).exists():
+                raise serializers.ValidationError({"phone": "Этот номер телефона уже используется."})
+
+            instance.user.phone = phone
+            instance.user.save()
+
+        instance.save()
+        return instance
--- src/employees/urls.py
+++ src/employees/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .views.employee_views import EmployeeListView, EmployeeDeleteView
 from .views.create_employee_views import EmployeeCreateView
 from .views.assigned_order_views import AssignedOrderEmployeeListView
+from .views.edit_employee_views import EmployeeUpdateView
 
 
 urlpatterns = [
     path('employees/', EmployeeListView.as_view(), name='employee-list'),
     path('create/', EmployeeCreateView.as_view(), name='employee-create'),
+    path('edit/<int:pk>', EmployeeUpdateView(), name='employee-edit'),
     path('<int:pk>/delete/', EmployeeDeleteView.as_view(), name='employee-delete'),
 
     path('assigned-orders/', AssignedOrderEmployeeListView.as_view(), name='assigned-orders-list'),
