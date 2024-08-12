428ed38a7750f52e967b23a460f79539bc87ba0a
tiw <tika11124@gmail.com>
2024-08-12 13:55:06 +0500

    fix / for urls edit-employee

--- src/employees/urls.py
+++ src/employees/urls.py
@@ -8,9 +8,8 @@ from .views.edit_employee_views import EmployeeUpdateView
 urlpatterns = [
     path('employees/', EmployeeListView.as_view(), name='employee-list'),
     path('create/', EmployeeCreateView.as_view(), name='employee-create'),
-    path('edit/<int:pk>', EmployeeUpdateView.as_view(), name='employee-edit'),
+    path('edit/<int:pk>/', EmployeeUpdateView.as_view(), name='employee-edit'),
     path('<int:pk>/delete/', EmployeeDeleteView.as_view(), name='employee-delete'),
 
     path('assigned-orders/', AssignedOrderEmployeeListView.as_view(), name='assigned-orders-list'),
-
 ]
