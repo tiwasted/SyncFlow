6f1d3cb742cbc63a112528992b0ddb8cb1f488a6
tiw <tika11124@gmail.com>
2024-09-05 16:18:20 +0500

    add: filter for City in order_history

--- src/order_history/filters.py
+++ src/order_history/filters.py
@@ -1,5 +1,6 @@
 import django_filters
 from b2c_client_orders.models import B2COrder
+from orders.models import City
 
 
 class B2COrderFilter(django_filters.FilterSet):
@@ -7,6 +8,7 @@ class B2COrderFilter(django_filters.FilterSet):
     order_date = django_filters.DateFilter(field_name='order_date', lookup_expr='exact')
     phone_number_client = django_filters.CharFilter(field_name='phone_number_client', lookup_expr='icontains')
     phone_number_employee = django_filters.CharFilter(field_name='assigned_employees__user__phone', lookup_expr='icontains')
+    city = django_filters.ModelChoiceFilter(queryset=City.objects.all(), field_name='city', lookup_expr='exact')
 
     # order_time = django_filters.TimeFilter(field_name='order_time', lookup_expr='exact')
     # order_name = django_filters.CharFilter(field_name='order_name', lookup_expr='icontains')
