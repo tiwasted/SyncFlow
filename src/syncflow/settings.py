88730c13f0f89e5fa2535cd20f78e5ca2bedbec9
tiw <tika11124@gmail.com>
2024-09-12 15:56:06 +0500

    fix: change configurate Django

--- src/syncflow/settings.py
+++ src/syncflow/settings.py
@@ -9,25 +9,20 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
-from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
 import os
 
-from tutorial.settings import BASE_DIR
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@q%=ctl)$$&v12+q@3k#p3@f$o-st0!02jx8*t6ml@zg4w1w&^'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
